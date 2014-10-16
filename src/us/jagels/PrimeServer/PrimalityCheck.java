1657f0f561421d6ac94e05f328431e0c65fda6d5
William <William@Jagels.us>
2014-10-15 20:34:58 -0400

    Alright Jenkins, let's see how quickly you see this!

---
+++ src/us/jagels/PrimeServer/PrimalityCheck.java
@@ -0,0 +1,14 @@
+package us.jagels.PrimeServer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrimalityCheck {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
